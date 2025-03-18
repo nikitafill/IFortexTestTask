336392dacef780df7d6b827b626ac643ec9d5271
Filchenko Nikita <filchenko.niki@mail.ru>
2025-03-19 00:20:52 +0300

    feat: add implementation of methods

--- Lab5TestTask/Services/Implementations/SessionService.cs
+++ Lab5TestTask/Services/Implementations/SessionService.cs
@@ -1,30 +1,40 @@
 ﻿using Lab5TestTask.Data;
+using Lab5TestTask.Enums;
 using Lab5TestTask.Models;
 using Lab5TestTask.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace Lab5TestTask.Services.Implementations;
-
-/// <summary>
-/// SessionService implementation.
-/// Implement methods here.
-/// </summary>
-public class SessionService : ISessionService
+namespace Lab5TestTask.Services.Implementations
 {
-    private readonly ApplicationDbContext _dbContext;
-
-    public SessionService(ApplicationDbContext dbContext)
+    /// <summary>
+    /// SessionService implementation.
+    /// </summary>
+    public class SessionService : ISessionService
     {
-        _dbContext = dbContext;
-    }
+        private readonly ApplicationDbContext _dbContext;
 
-    public async Task<Session> GetSessionAsync()
-    {
-        throw new NotImplementedException();
-    }
+        public SessionService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
 
-    public async Task<List<Session>> GetSessionsAsync()
-    {
-        throw new NotImplementedException();
+        public async Task<Session> GetSessionAsync()
+        {
+            return await _dbContext.Sessions
+                .Where(s => s.DeviceType == DeviceType.Desktop)
+                .OrderBy(s => s.StartedAtUTC)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Session>> GetSessionsAsync()
+        {
+            return await _dbContext.Sessions
+                .Where(s => s.User.Status == UserStatus.Active && s.EndedAtUTC.Year < 2025)
+                .ToListAsync();
+        }
     }
 }
